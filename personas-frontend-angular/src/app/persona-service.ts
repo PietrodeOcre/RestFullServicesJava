7cbcf489dc0ae0c97be4642b2be998040165e057
PietrodeOcre <pietrodeocre@gmail.com>
2020-09-03 21:47:58 +0200

    Ultimo cambio ya funciona

--- personas-frontend-angular/src/app/persona-service.ts
+++ personas-frontend-angular/src/app/persona-service.ts
@@ -5,48 +5,50 @@ import { DataService } from './data-service';
 @Injectable()
 export class PersonaService {
 
-  public personas: Persona[] = [];
+  personas: Persona[] = [];
 
-  constructor(private dataService: DataService) { }
+  constructor( private dataService: DataService) {}
 
   // Se usa para modificar el valor del arreglo debido a la llamada asincrona
   // tslint:disable-next-line: typedef
-  public setPersonas(personas: Persona[]) {
+  setPersonas(personas: Persona[]){
     this.personas = personas;
   }
 
   // tslint:disable-next-line: typedef
-  public obtenerPersonas() {
+  obtenerPersonas() {
     return this.dataService.cargarPersonas();
   }
 
+
   // tslint:disable-next-line: typedef
-  public agregarPersona(persona: Persona) {
-    console.log('Persona a agregar: ' + persona.nombre);
-    this.dataService.agregarPersona(persona).subscribe(
-      // tslint:disable-next-line: no-shadowed-variable
-      (persona: Persona) => {
-        // Recuperamos el objeto persona con el idPersona recien agregado
-        console.log('Se agrega la persona insertada al arreglo a traves del subcriber: ' + persona.idPersona);
-        this.personas.push(persona);
-      }
-    );
+  agregarPersona(persona: Persona){
+    console.log('persona a agregar:' + persona.nombre);
+    this.dataService.agregarPersona(persona)
+      .subscribe(
+        // tslint:disable-next-line: no-shadowed-variable
+        (persona: Persona) => {
+          // Recuperamos objeto Persona con el idPersona recien agregado
+          console.log('se agrega al arreglo la persona recien insertada suscriber:' + persona.idPersona);
+          this.personas.push(persona);
+        }
+      );
   }
 
   // tslint:disable-next-line: typedef
-  public encontrarPersona(id: number) {
+  encontrarPersona(id: number){
     // tslint:disable-next-line: no-shadowed-variable
-    const persona: Persona = this.personas.find(persona => persona.idPersona === id);
+    const persona: Persona = this.personas.find( persona => persona.idPersona == id);
     console.log('persona encontrada:' + persona.idPersona + ' ' + persona.nombre);
     return persona;
   }
 
   // tslint:disable-next-line: typedef
-  public modificarPersona(id: number, persona: Persona) {
+  modificarPersona(id: number, persona: Persona) {
     console.log('persona a modificar:' + persona.idPersona);
-    // Se actualiza el objeto persona del arreglo
+    // se actualiza el objeto persona del arreglo
     // tslint:disable-next-line: no-shadowed-variable
-    const personaModificadaLocal = this.personas.find(persona => persona.idPersona === id);
+    const personaModificadaLocal = this.personas.find(persona => persona.idPersona == id);
     personaModificadaLocal.idPersona = persona.idPersona;
     personaModificadaLocal.nombre = persona.nombre;
     // guardar la persona en la base de datos
@@ -54,12 +56,10 @@ export class PersonaService {
   }
 
   // tslint:disable-next-line: typedef
-  public eliminarPersona(id: number) {
+  eliminarPersona(id: number) {
     console.log('eliminar persona con id:' + id);
-    const index = this.personas.findIndex(persona => persona.idPersona === id); // encontramos el indice en el arreglo
+    const index = this.personas.findIndex( persona => persona.idPersona == id); // encontramos el indice en el arreglo
     this.personas.splice(index, 1);
     this.dataService.eliminarPersona(id);
   }
-
-
 }
