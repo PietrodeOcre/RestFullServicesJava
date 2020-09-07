4f00cc3c02848596c968b38dda41f5e7c279828e
pietrodeocre <pietrodeocre@gmail.com>
2020-09-07 19:07:34 +0200

    Prueba de java

---
+++ Holamundo/src/main/java/com/nukerlabs/holamundo/HolaMundo.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.nukerlabs.holamundo;
+
+/**
+ *
+ * @author pietrodeocre
+ */
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundodesde Java");
+    }
+}
